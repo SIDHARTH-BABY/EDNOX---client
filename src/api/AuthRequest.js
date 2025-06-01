69ce875d898ee3d4eaa1accfd87f9792ef264f97
sidharthbaby77@gmail.com <sidharthbaby77@gmail.com>
2025-06-01 17:11:17 +0530

    redirect

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
+// const API = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
+const API = axios.create({ baseURL: '/api' });
 
 
 export const sendOtp = (email) => API.post("/send-otp", { email })
